14d8a41dca77c312fdecab6aa076b3ee291f9e95
Waseeq <waseeqnazir@gmail.com>
2025-06-06 17:50:42 +0500

    Initial working app

--- lib/db/initialize-rx-db.ts
+++ lib/db/initialize-rx-db.ts
@@ -1,29 +1,29 @@
-import 'react-native-get-random-values'
-import { createRxDatabase, addRxPlugin } from 'rxdb'
-import { RxDBQueryBuilderPlugin } from 'rxdb/plugins/query-builder'
-import { RxDBUpdatePlugin } from 'rxdb/plugins/update'
-import { wrappedValidateAjvStorage } from 'rxdb/plugins/validate-ajv'
-import { getRxStorageMemory } from 'rxdb/plugins/storage-memory'
-import CryptoJS from 'crypto-js'
-import { collections } from './schemas'
+import "react-native-get-random-values";
+import { createRxDatabase, addRxPlugin } from "rxdb";
+import { RxDBQueryBuilderPlugin } from "rxdb/plugins/query-builder";
+import { RxDBUpdatePlugin } from "rxdb/plugins/update";
+import { wrappedValidateAjvStorage } from "rxdb/plugins/validate-ajv";
+import { getRxStorageMemory } from "rxdb/plugins/storage-memory";
+import CryptoJS from "crypto-js";
+import { collections } from "./schemas";
 
-addRxPlugin(RxDBQueryBuilderPlugin)
-addRxPlugin(RxDBUpdatePlugin)
+addRxPlugin(RxDBQueryBuilderPlugin);
+addRxPlugin(RxDBUpdatePlugin);
 
 const customHashFunction = (input: string): Promise<string> => {
-	const hash = CryptoJS.SHA256(input).toString(CryptoJS.enc.Hex)
-	return Promise.resolve(hash)
-}
+  const hash = CryptoJS.SHA256(input).toString(CryptoJS.enc.Hex);
+  return Promise.resolve(hash);
+};
 
 export const initializeRxDb = async () => {
-	const db = await createRxDatabase({
-		name: 'youcraft-mobile',
-		storage: wrappedValidateAjvStorage({
-			storage: getRxStorageMemory(),
-		}),
-		multiInstance: false,
-		hashFunction: customHashFunction,
-	})
-	await db.addCollections(collections)
-	return db
-}
+  const db = await createRxDatabase({
+    name: "recalibration-app",
+    storage: wrappedValidateAjvStorage({
+      storage: getRxStorageMemory(),
+    }),
+    multiInstance: false,
+    hashFunction: customHashFunction,
+  });
+  await db.addCollections(collections);
+  return db;
+};
